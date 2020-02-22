1613d09ffb5ac51584bfb057e2c5fde1f730eda2
pnthompson <pnthompson@gmail.com>
2020-02-22 13:26:03 -0700

    lazyload

---
+++ lesson7/js/lazyload.js
@@ -0,0 +1,24 @@
+//get all images with data-src attribute
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+//optional parameters being set for the IntersectionObserver
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.gettAttribute('data-src'));
+    image.onload = () => { image.removeAttribute('data-src'); };
+};
+
+//first check to see if Intersection Observer is supported
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {});
+    }, imgOptions);
+
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+} else { // just load All images if not supported..active
+}
