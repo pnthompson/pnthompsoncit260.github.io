bf4dabed7ac01df106cdb892c34146f4aaa53248
pnthompson <pnthompson@gmail.com>
2020-03-01 22:10:09 -0700

    sc css

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -2,8 +2,8 @@
 const imagesToLoad = document.querySelectorAll("img[data-src]");
 //optional parameters being set for the IntersectionObserver
 const imgOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px 50px 0px"
+    threshold: 1,
+    rootMargin: "0px 0px 200px 0px"
 };
 
 const loadImages = (image) => {
